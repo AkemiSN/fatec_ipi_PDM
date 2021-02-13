12d3d92c06f3b96ebcd32ea66743131a3191b029
Akemi <61330439+AkemiSN@users.noreply.github.com>
2021-02-12 22:59:27 -0300

    Version 1

---
+++ Exercicio/EntregaGit.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+
+public class EntregaGit {
+    public static void main(String[] args) {
+        String nome = JOptionPane.showInputDialog("Qual seu nome");
+        JOptionPane.showMessageDialog(null, String.format("Bem vindo, %s", nome));
+    }
+}
